eef013554a76d38fc6da0990da6f233121676065
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-24 15:37:47 -0400

    commit tree ea18b7ecd3afdbf581aea77a42e09d18d2447fcf

--- index.js
+++ index.js
@@ -8,7 +8,81 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+let createEmployeeRecord = (empArray) => {
+  const empRecord = {
+    firstName: empArray[0],
+    familyName: empArray[1],
+    title: empArray[2],
+    payPerHour: empArray[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return empRecord;
+}
 
+let createEmployeeRecords = (empArrays) => {
+  return empArrays.map(empArray => createEmployeeRecord(empArray));
+}
+
+// their code below:
+//let createTimeInEvent = function(dateStamp){
+//    let [date, hour] = dateStamp.split(' ')
+//
+//    this.timeInEvents.push({
+//        type: "TimeIn",
+//        hour: parseInt(hour, 10),
+//        date,
+//    })
+//
+//    return this
+//}
+//
+//let createTimeOutEvent = function(dateStamp){
+//    let [date, hour] = dateStamp.split(' ')
+//
+//    this.timeOutEvents.push({
+//        type: "TimeOut",
+//        hour: parseInt(hour, 10),
+//        date,
+//    })
+//
+//    return this
+//}
+
+  let createTimeInEvent = function(timeIn) {
+    let [date, hour] = timeIn.split(' ');
+    this.timeInEvents.push({type: "TimeIn", date: date, hour: parseInt(hour, 10)});
+    return this
+  }
+  
+  let createTimeOutEvent = function(timeOut) {
+    let [date, hour] = timeOut.split(' ');
+    this.timeOutEvents.push({type: "TimeOut", date: date, hour: parseInt(hour, 10)});
+    return this
+  }
+
+let hoursWorkedOnDate = function(dateStr) {
+  let timeIn = this.timeInEvents.find( (e) => { return e.date === dateStr});
+  let timeOut = this.timeOutEvents.find( (e) => { return e.date === dateStr});
+  return (timeOut.hour - timeIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateStr) {
+  let wage = hoursWorkedOnDate.call(this, dateStr) * this.payPerHour;
+  return parseFloat(wage.toString())
+}
+
+let findEmployeeByFirstName = (empRecords, firstName) => {
+  return empRecords.find( (empRecord) => {return empRecord.firstName === firstName});
+}
+
+let calculatePayroll = function(empRecords) {
+    return empRecords.reduce( function(accumulator, empRecord) { return accumulator + allWagesFor.call(empRecord)}, 0)
+}
+
+
+
+// their code below this line
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
